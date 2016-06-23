61de0d54efb589ace320a6200577c22de170ab06
CarloCloudEmployee <carlo@cloudemployee.co.uk>
2016-06-23 22:08:31 +0800

    Slider - layout of Slider for Desktop

--- resources/js/src/slider.js
+++ resources/js/src/slider.js
@@ -2,9 +2,14 @@ import $ from "./../vendor/jquery/dist/jquery";
 import slick from "./../vendor/slick/rdslick.js";
 
 const slider = () => {
-	
-	$(".hero-slider").slick({
-		dots: true, infinite: true, speed: 300, slidesToShow: 1, centerMode: false
+
+	$(".slider__items").slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		dots: true,
+		arrows: false
 	});
 
 };

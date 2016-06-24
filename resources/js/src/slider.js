1a13552c971f0c932de2e408352ca54d86ef2c2e
CarloCloudEmployee <carlo@cloudemployee.co.uk>
2016-06-24 19:51:12 +0800

    Sidebar - Added the content layout for the sidebar.

--- resources/js/src/slider.js
+++ resources/js/src/slider.js
@@ -6,7 +6,7 @@ const slider = () => {
 	$(".slider__items").slick({
 		slidesToShow: 3,
 		slidesToScroll: 3,
-		autoplay: true,
+		//autoplay: true,
 		autoplaySpeed: 4000,
 		dots: true,
 		arrows: false

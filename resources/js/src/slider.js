7a00b197e08bbfe59f05d6a0f375e40dedbf89dd
CarloCloudEmployee <carlo@cloudemployee.co.uk>
2016-06-27 22:25:04 +0800

    Update overall layout

--- resources/js/src/slider.js
+++ resources/js/src/slider.js
@@ -6,10 +6,30 @@ const slider = () => {
 	$(".slider__items").slick({
 		slidesToShow: 3,
 		slidesToScroll: 3,
-		//autoplay: true,
+		autoplay: true,
 		autoplaySpeed: 4000,
 		dots: true,
-		arrows: false
+		arrows: false,
+		responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+				arrows: true,
+				dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+				arrows: true,
+				dots: false
+      }
+    }
+  ]
 	});
 
 };
